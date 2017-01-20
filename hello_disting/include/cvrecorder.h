6365f0e224580ae45aab4dc752381e1130b33d33
seb <sebastian.sjogren@gmail.com>
2017-01-20 19:21:03 +0100

    Begin work on linerp for cvrecord

--- hello_disting/include/cvrecorder.h
+++ hello_disting/include/cvrecorder.h
@@ -5,23 +5,40 @@
 #include "leds.h"
 
 #define CVBUF_SIZE (6 * 4096)
-#define CVBUF_END (CVBUF_SIZE - 1)
 
-static short __cvbuf[CVBUF_SIZE];
+static fix16 __cvbuf[CVBUF_SIZE];
 
 static int __subsample_factor = 1;
-
-static int __head = 0;
 static int __count = 0;
 
-void inline cvRecord(int x, int y) {
-    *(__cvbuf + __head) = (short) (x >> 8);
-    *(__cvbuf + __head + 1) = (short) (y >> 8);
+static int __head = 0;
+
+void inline cvRecord(register fix16 x, register fix16 y) {
+    register short* pWrite = __cvbuf + __head;
+
+    *pWrite = x;
+    *(pWrite + 1) = y;
 }
 
-void inline cvPlay(int* x, int* y) {
-    *x = *(__cvbuf + __head) << 8;
-    *y = *(__cvbuf + __head + 1) << 8;
+void inline cvPlay(register fix32* x, register fix32* y) {
+    register fix16* pw = __cvbuf + __head;
+
+    *x = *pw << 8;
+    *y = *(pw + 1) << 8;
+
+    
+    // TODO: linear interpolation
+    
+    register short next_x;
+    register short next_y;
+
+    if (__head < CVBUF_SIZE - 3) {
+        next_x = *(pw + 2);
+        next_y = *(pw + 3);
+    } else {
+        next_x = *__cvbuf;
+        next_y = *(__cvbuf + 1);
+    }
 }
 
 void doCvRecorder(int sub) {
@@ -33,8 +50,8 @@ void doCvRecorder(int sub) {
         // wait for new audio frame
         IDLE();
 
-        int x = inX;
-        int y = inY;
+        fix16 x = inX >> 8;
+        fix16 y = inY >> 8;
 
         // do the processing
         //doLeds(y);
@@ -47,14 +64,14 @@ void doCvRecorder(int sub) {
             } else {
 
             }
-            cvPlay(&outA, &outB);
             __head += 2;
 
-            if (__head >= CVBUF_END) {
+            if (__head > CVBUF_SIZE - 2) {
                 __head = 0;
             }
-
         }
+
+        cvPlay(&outA, &outB);
 
         // loop end processing
         // (including reading the ADC channels)
--- hello_disting/include/fixed.h
+++ hello_disting/include/fixed.h
@@ -2,10 +2,16 @@
 #define FIXED_H_INCLUDED
 
 typedef signed int fix32 ;
+typedef signed short fix16;
+
+#define MAX_30 (1073741824)
 
 #define multfix32(a,b) ((fix32)(((( signed long long)(a))*(( signed long long)(b))) >> 30)) //multiply two fixed 2:30
+#define divfix32(a,b) ((fix32)((((signed long long)(a)<<30)/(b))))
 
-//#define float2fix32(a) ((fix32)((a)*1073741824.0)) // 2^30
-//#define fix2float32(a) ((float)(a)/1073741824.0)
+#define float2fix32(a) ((fix32)((a)*1073741824.0)) // 2^30
+#define fix2float32(a) ((float)(a)/1073741824.0)
+
+#define linterp(weight, start, end) ((multfix32(MAX_30 - weight, start) + multfix32(weight, end)))
 
 #endif // FIXED_H_INCLUDED
